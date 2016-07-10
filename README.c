69a6fbcb85c11af247beb2cb0d09f587e07553ca
divinivi <divimadhu23@gmail.com>
2016-07-10 12:30:31 +0530

    finish

--- README.c
+++ README.c
@@ -6,13 +6,11 @@ void towerofhanoi(int num,char frompeg,char topeg,char auxpeg){
         return;
     }
     towerofhanoi(num-1,frompeg,auxpeg,topeg);
-    printf("\nMoving disc %d from peg %c to peg %c",num,frompeg,topeg);
     towerofhanoi(num-1,topeg,frompeg,auxpeg);
 }
 
 int main(){
     int num;
     scanf("%d",&num);
-    towerofhanoi(num,'A','C','B');
     return 0;
 }
